7fd4a6f3409b030236ddf263ffb28d73b7fd896f
StratoLin <darwiguille@gmail.com>
2024-02-24 16:20:35 -0500

    Inicio proyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { UsuarioComponent } from './components/usuario/usuario.component';
+import { FormularioComponent } from './components/formulario/formulario.component';
+import { PagesMainComponent } from './components/pages-main/pages-main.component';
+import { RegistroComponent } from './components/registro/registro.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    UsuarioComponent,
+    FormularioComponent,
+    PagesMainComponent,
+    RegistroComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/pages-main/pages-main.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pages-main',
+  templateUrl: './pages-main.component.html',
+  styleUrls: ['./pages-main.component.css']
+})
+export class PagesMainComponent {
+
+}
