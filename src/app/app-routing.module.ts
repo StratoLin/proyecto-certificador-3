b08ecb0beb7d31a9c3bc19e84a8ff5b42c365110
StratoLin <darwiguille@gmail.com>
2024-02-24 21:04:02 -0500

    se agrega footer y pantalla de inicio

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PagesMainComponent } from './components/pages-main/pages-main.component';
+import { RegistroComponent } from './components/registro/registro.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'registro', component: RegistroComponent},
+  {path: 'inicio', component: PagesMainComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,25 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { UsuarioComponent } from './components/usuario/usuario.component';
-import { FormularioComponent } from './components/formulario/formulario.component';
 import { PagesMainComponent } from './components/pages-main/pages-main.component';
 import { RegistroComponent } from './components/registro/registro.component';
+import { LoginComponent } from './components/login/login.component';
+import { ProductosComponent } from './components/productos/productos.component';
+import { CarritoComponent } from './components/carrito/carrito.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     UsuarioComponent,
-    FormularioComponent,
+
     PagesMainComponent,
-    RegistroComponent
+    RegistroComponent,
+    LoginComponent,
+    ProductosComponent,
+    CarritoComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
